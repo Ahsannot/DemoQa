622457071390f49c7ed3936f080aece13a12f6b6
HafizMuhamma_0x5fj80 <hafizgee07@gmail.com>
2025-12-26 10:47:06 +0500

    Develop reporting class complete alongwith ScreenShot implementation in I testListner class and create helper method in BaseClass to get driver that used in Listner class for reporting

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -9,11 +9,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.time.Duration;
 import java.util.Properties;
 
@@ -65,6 +68,19 @@ public class BaseClass {
 
     public String randomAlphaNumeric() {
         return RandomStringUtils.randomAlphanumeric(8); // e.g., "A1B2C3D4"
+    }
+
+
+    // Attach driver to test result attributes before each test method for reporting
+    // It runs even if the test belongs to a skipped group
+    @BeforeMethod(alwaysRun = true)
+    // Method method (Represents the test method about to run)  ITestResult result (Test status, Attributes, Exceptions)
+    public void attachDriverToTestResult(Method method, ITestResult result) {
+        // Attaches the WebDriver instance to the current test result
+        //Stored as a key–value pair:
+        //Key: "driver"
+        //Value: driver
+        result.setAttribute("driver", driver);
     }
 
     @AfterClass
