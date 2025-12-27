6746ade0afa8c5e5048493b716f56560403748e4
HafizMuhamma_0x5fj80 <hafizgee07@gmail.com>
2025-12-27 21:30:55 +0500

    modify RegisterDataProvider class

--- src/test/java/utilities/RegisterDataProvider.java
+++ src/test/java/utilities/RegisterDataProvider.java
@@ -23,9 +23,9 @@ public class RegisterDataProvider {
         String excelPath = ConfigReader.getProperty("excel_User_Register_Path");
         String sheetName = ConfigReader.getProperty("register_sheet_name");
 
-        System.out.println("Reading Register data from Excel: " + excelPath + ", Sheet: " + sheetName);
         logger.info("Reading Register data from Excel: {}, Sheet: {}", excelPath, sheetName);
 
+        // declared outside the try block / assigned inside the try / used in the finally block
         ExcelUtils excel = null;
         Object[][] data = null;
 
@@ -45,19 +45,18 @@ public class RegisterDataProvider {
                 data[i - 1][4] = excel.getCellData(i, 4); // ExpectedResult
                 data[i - 1][5] = i;                       // RowIndex (optional for tracking)
 
+                // INFO Loaded Row 3 -> UserName: john_doe, ExpectedResult: SUCCESS
                 logger.info("Loaded Row {} -> UserName: {}, ExpectedResult: {}", i, data[i - 1][2], data[i - 1][4]);
 
             }
 
         } catch (Exception e) {
-            System.out.println("Error reading Register data: " + e.getMessage());
             logger.error("Failed to read Register data from Excel", e);
             throw e;
         } finally {
             // Close workbook to release file resources
             if (excel != null) {
                 excel.closeWorkbook();
-                System.out.println("Excel workbook closed.");
                 logger.info("Excel workbook closed.");
             }
         }
