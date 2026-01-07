f1985196a8ba05d3433d086f3fe66f15eb1e5316
HafizMuhamma_0x5fj80 <hafizgee07@gmail.com>
2026-01-07 20:45:30 +0500

    TextBoxPage implemented

--- src/test/java/pageObjects/TextBoxPage.java
+++ src/test/java/pageObjects/TextBoxPage.java
@@ -1,10 +1,52 @@
 package pageObjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class TextBoxPage extends BasePage{
 
     public TextBoxPage(WebDriver driver){
         super(driver);
+    }
+
+    //   ************************ LOCATORS ************************
+
+    @FindBy(xpath = "//input[@id='userName']")
+    WebElement inputUserName;
+
+    @FindBy(xpath = "//input[@id='userEmail']")
+    WebElement inputUserEmail;
+
+    @FindBy(xpath = "//textarea[@id='currentAddress']")
+    WebElement inputCurrentAddress;
+
+    @FindBy(xpath = "//textarea[@id='permanentAddress']")
+    WebElement inputPermanentAddress;
+
+    @FindBy(xpath = "//button[@id='submit']")
+    WebElement btnSubmit;
+
+
+    //   ************************ ACTION METHODS ************************
+
+    public void enterUserName(String userName){
+        inputUserName.sendKeys(userName);
+    }
+
+    public void enterUserEmail(String userEmail){
+        inputUserEmail.sendKeys(userEmail);
+    }
+
+    public void enterCurrentAddress(String currentAddress){
+        inputCurrentAddress.sendKeys(currentAddress);
+    }
+
+    public void enterPermanentAddress(String permanentAddress){
+        inputPermanentAddress.sendKeys(permanentAddress);
+    }
+
+    public void clickSubmitBtn(){
+        btnSubmit.click();
     }
 }
